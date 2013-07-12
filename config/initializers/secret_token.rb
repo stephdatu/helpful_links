dd3118fe72bc04d6a05545e13d8b754edc1589fc
Stephanie Datu <stephdatu@gmail.com>
2013-07-11 21:49:40 -0700

    Generate Links App using scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelpfulLinks::Application.config.secret_token = '612ee9dd88f504302afadee62c82ce713a922b24db60a8f23dceb2b04b25effdec5538de3c747c477b13f8eb6f08ea7399474790ce002a0beb399680ea5c11aa'
